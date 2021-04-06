48738c29ccb54d18c750b14d63c5d688205756d8
obddd <obaidulhaq20@gmail.com>
2021-04-06 23:25:47 +0500

    created footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,29 @@
+import React from "react"
+import * as styles from "./footer.module.css"
+import { Link } from "gatsby"
+import links from "../../constants/links"
+import socialLinks from "../../constants/social-links"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((item, i) => (
+          <Link key={i} to={item.path}>
+            {item.text}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        {socialLinks.map((item, i) => (
+          <a key={i} href={item.url} target="_blank" rel="noreferrer">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>copyrights &copy; in transit company {new Date().getFullYear()} all rights reserved</div>
+    </footer>
+  )
+}
+
+export default Footer
