99708f10191ffe1895588b7a8f1332284101b829
obddd <obaidulhaq20@gmail.com>
2021-04-07 23:34:23 +0500

    created error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Layout from '../components/layout/Layout'
+import React from "react"
+import Banner from "../components/banner/Banner"
+import Layout from "../components/layout/Layout"
+import * as styles from "../css/error.module.css"
+import {Link} from 'gatsby'
 
 const error = () => {
-    return (
-        <Layout>
-            Hello from error page!
-        </Layout>
-    );
-};
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="the page doesn't exists">
+          <Link to="/" className="btn-white">
+            go to home
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
+}
 
-export default error;
+export default error
